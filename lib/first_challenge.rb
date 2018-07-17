92360d4eb2a135d8efd09f80a1cc767888a8e27f
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-17 17:42:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,7 @@ def first_challenge
       data.each do |attribute, value|
         if attribute = :favorite_icecream_flavors
           :favorite_icecream_flavors.delete_if do |flavor|
-            favor == "strawbery"
+            flavor == "strawbery"
           
         end  
         
